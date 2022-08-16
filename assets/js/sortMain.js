b764c1619efbe4aa0d929e511f6b60980b2eb104
Siwu Li <88387067+siwu-945@users.noreply.github.com>
2022-08-16 00:42:08 -0400

    Update sortMain.js

--- assets/js/sortMain.js
+++ assets/js/sortMain.js
@@ -3,27 +3,6 @@ var div_sizes = [];
 var divs = [];
 //var cont = document.getElementsById("array");
 
-function generateInitialArray1(){
-    for (var i = 0; i < sizeNum; i++) {
-        var value = Math.ceil(Math.random() * 100);
-
-        divs[i] = document.createElement("div");
-
-        divs[i].classList.add("block");
-
-        divs[i].style.height = `${value * 3}px`;
-        divs[i].style.transform = `translate(${i * 30}px)`;
-
-        var array_ele_label = document.createElement("label");
-        divs[i].classList.add("block_id");
-        divs[i].innerText = value;
-
-        divs[i].appendChild(array_ele_label);
-        container.appendChild(divs[i]);
-    }
-
-}
-
 // Function to generate the array of blocks
 function generateInitialArray() {
     for (var i = 0; i < sizeNum; i++) {
@@ -50,10 +29,45 @@ function generateInitialArray() {
         // Appending created elements to index.html
         array_ele.appendChild(array_ele_label);
         container.appendChild(array_ele);
-
     }
 }
 
+function generateSortedArray(){
+    var sorted = [];
+
+    for(var i = 0; i<sizeNum; i++){
+        var innerVal = Number(Math.ceil(Math.random()*100));
+        sorted.push(innerVal);
+    }
+
+    sorted.sort(function (a, b){
+        return a-b;
+    });
+
+    for (var i = 0; i < 20; i++) {
+        var value = sorted[i];
+
+        // Creating element div
+        var array_ele = document.createElement("div");
+
+        // Adding class 'block' to div
+        array_ele.classList.add("block");
+
+        // Adding style to div
+        array_ele.style.height = `${value * 3}px`;
+        array_ele.style.transform = `translate(${i * 30}px)`;
+
+        // Creating label element for displaying
+        // size of particular block
+        var array_ele_label = document.createElement("label");
+        array_ele_label.classList.add("block_id");
+        array_ele_label.innerText = value;
+
+        // Appending created elements to index.html
+        array_ele.appendChild(array_ele_label);
+        container.appendChild(array_ele);
+    }
+}
 
 function deletePreviousArray(){
     for (var i = 0; i < divs.length; i++) {
@@ -61,11 +75,11 @@ function deletePreviousArray(){
     }
 }
 
-
 function updateArray(){
     deletePreviousArray();
     sizeNum = document.getElementById("arraySize").value;
-    generateInitialArray();
+    //generateInitialArray();
+    generateSortedArray();
 }
 
 window.onload = updateArray();
