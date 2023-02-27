2a56d2149f965acf68f49b8be91ef6d5c919476d
Siwu Li <siwu028@goxmail.com>
2023-02-26 21:09:18 -0500

    popupWindow

---
+++ assets/js/blurry.js
@@ -0,0 +1,17 @@
+$(function() {
+    $('.pop-up').hide();
+    $('.pop-up').fadeIn(1000);
+
+    $('.close-button').click(function(e) {
+        $('.pop-up').fadeOut(700);
+        $('#overlay').removeClass('blur-in');
+        $('#overlay').addClass('blur-out');
+        e.stopPropagation();
+    });
+
+    $('#begin').click(function() {
+        $('.pop-up').fadeOut(700);
+        $('#overlay').removeClass('blur-in');
+        $('#overlay').addClass('blur-out');
+    });
+});
